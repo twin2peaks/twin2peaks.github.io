d242e892e203d600f34acc8ce6b7dedae7cba83c
SamWieHTWG <wiertzsamuel@gmail.com>
2026-01-29 08:56:07 +0100

    update to v2.1.21

--- _app/immutable/nodes/4.CB15M_gx.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/0VSHVk5S.js";export{e as component,n as universal};
---
+++ _app/immutable/nodes/4.CnV4ltlS.js
@@ -0,0 +1,1 @@
+import{a as e,_ as n}from"../chunks/QF9OyLpf.js";export{e as component,n as universal};
