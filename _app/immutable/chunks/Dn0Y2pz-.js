184f5ff931f830d07cc0edf74a9aa7a15b840814
SamWieHTWG <wiertzsamuel@gmail.com>
2026-03-25 11:45:13 +0100

    update to v2.1.77

---
+++ _app/immutable/chunks/Dn0Y2pz-.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_113gn7d?.base??"",a=globalThis.__sveltekit_113gn7d?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/JbEL70Ai.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1bcq4ns?.base??"",a=globalThis.__sveltekit_1bcq4ns?.assets??s??"";export{a,s as b};
